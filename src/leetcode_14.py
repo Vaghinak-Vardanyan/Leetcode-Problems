3c7d0a5f919a36b037ba6c2c996789d847677bee
Vaghinak Vardanyan <vaghinak.vardanyan@technamin.com>
2022-02-15 01:35:11 +0400

    leetcode 14

---
+++ src/leetcode_14.py
@@ -0,0 +1,24 @@
+
+# 14. Longest Common Prefix
+
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        result: str = ""
+
+        first: str = strs[0]
+        for index, char in enumerate(first):
+            not_prefix: bool = False
+            for word in strs[1:]:
+                if index >= len(word) or word[index] != char:
+                    not_prefix = True
+                    break
+            
+            if not_prefix:
+                break
+            result += char
+
+        return result
+
+print(Solution().longestCommonPrefix(["alower","flow","flight"]))
