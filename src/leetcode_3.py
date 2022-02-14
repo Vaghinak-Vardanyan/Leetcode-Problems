0ed2008b886e0c24998e39ead98e99bfad5679f0
Vaghinak Vardanyan <vaghinak.vardanyan@technamin.com>
2022-02-14 15:00:06 +0400

    improvement

--- src/leetcode_3.py
+++ src/leetcode_3.py
@@ -4,30 +4,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
-        if not s:
-            return 0
-        
-        length: int = len(s)
-        result: int = 1
-        
-        for i in range(2, length + 1):
-            window: dict[str, int] = {}
-            start: int = 0
-            end: int = i
+        result: int = 0
+        window: list[int | None] = [None] * 128
+        left: int = 0
+        right: int = 0
 
-            while start < end:
-                if s[start] not in window:
-                    window[s[start]] = start
-                    start += 1
-                else:
-                    start = window[s[start]] + 1
-                    window.clear()
-                    end = start + i
-                    if end > length:
-                        return result
-            
-            result = i
+        while right < len(s):
+
+            current = s[right]
+
+            index = window[ord(current)]
+            if index != None and left <= index < right:
+                left = index + 1
+
+            result = max(result, right - left + 1)
+
+            window[ord(current)] = right
+            right += 1
 
         return result
 
-print(Solution().lengthOfLongestSubstring("aab"))
+print(Solution().lengthOfLongestSubstring("au"))
